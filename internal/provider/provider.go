0b55ff074fa7a881aedf4aef9aeca22cea4a413b
Marcus Weiner <marcus.weiner@gmail.com>
2025-11-18 20:24:09 +0100

    Generate session datasources

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -23,6 +23,7 @@ import (
 )
 
 //go:generate go tool tfplugingen-framework generate resources --input ../../client/resource_spec.patched.json  --output ../
+//go:generate go tool tfplugingen-framework generate data-sources --input ../../client/resource_spec.patched.json  --output ../
 
 //go:generate go run ./gen
 
